e13de0f60d351ceaf52a4b29cd9a9bf401ca4bf0
Railey Pacheco <111040098+Railey29@users.noreply.github.com>
2025-04-16 22:44:09 -0800

    Update PORT

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import threading
 from flask import Flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -25,4 +26,5 @@ def starting_game():
     return "Game started"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
